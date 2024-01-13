65af72ae241d50ce7ee1ad0efbcedba950c5c21e
JoshiRishikesh <89295377+JoshiRishikesh@users.noreply.github.com>
2024-01-13 16:38:27 +0000

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "rishikeshjoshi9546@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+// accountId = 2 // not allowed
+accountEmail = "rishi.com"
+accountPassword = "1112224"
+accountCity = "Dapodi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"Use Strict"; // treat all JS code as newer version
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3+3); console.log("Rishi"); // code readibility should be high this is not a goof practice.
+
+let name =  "Rishi"
+let age = 20
+let isLoggedIn = true
+// null => it is a standalone value, it is a representation of empty value
+// undefined => when value is not defined
+// symbol => Unique
+// object
+// console.log(typeof"Risghi); // it is a string type
