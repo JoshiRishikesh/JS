836b4803ed9cd8ac818500267acde7847f9952f6
JoshiRishikesh <89295377+JoshiRishikesh@users.noreply.github.com>
2024-01-14 19:10:42 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// node 01_basics/04_comparision.js
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+
+// console.log("2"===2);
