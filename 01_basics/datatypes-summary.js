38cfff286380e52e721e49240c82d360e60d57da
JoshiRishikesh <89295377+JoshiRishikesh@users.noreply.github.com>
2024-01-14 19:32:59 +0000

    datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,14 @@
+// premitive
+
+// 7 types:- String, Number, Boolean, Undefined, null, Symbol, BigInt
+
+// Refrence (Non - Premetive):- Array, Objects, Functions
+const heros = ["Shaktiman", "Nagraj", "Doga"]
+let myObj = {
+    name: "Rishi",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
