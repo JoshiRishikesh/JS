ba91e6687ae0b72579b20ecc336aeec20d1f4319
JoshiRishikesh <89295377+JoshiRishikesh@users.noreply.github.com>
2024-01-16 18:33:32 +0000

    done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,14 +1,39 @@
-// premitive
+// // // premitive
+// // node 01_basics/datatypes-summary.js
 
-// 7 types:- String, Number, Boolean, Undefined, null, Symbol, BigInt
+// // // 7 types:- String, Number, Boolean, Undefined, null, Symbol, BigInt
 
-// Refrence (Non - Premetive):- Array, Objects, Functions
-const heros = ["Shaktiman", "Nagraj", "Doga"]
-let myObj = {
-    name: "Rishi",
-    age: 22,
+// // // Refrence (Non - Premetive):- Array, Objects, Functions
+// // const heros = ["Shaktiman", "Nagraj", "Doga"]
+// // let myObj = {
+// //     name: "Rishi",
+// //     age: 22,
+// // }
+
+// // const myFunction = function(){
+// //     console.log("Hello World");
+// // }
+
+
+// // Stack (Primitive), Heap (Non Primitive)
+
+// let myYoutubeName = "Rishijoshi"
+// let anotherName = myYoutubeName
+
+// console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+
 }
 
-const myFunction = function(){
-    console.log("Hello World");
-}
+let userTwo = userOne
+
+userTwo.email = "rishi@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
+// ***************************************************************************************************************************
