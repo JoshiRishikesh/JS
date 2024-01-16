0d66436b19e6387235a1bc6c4b3f66085596a4e1
JoshiRishikesh <89295377+JoshiRishikesh@users.noreply.github.com>
2024-01-16 20:09:22 +0000

    math & Num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+// // node 01_basics/06_nums_and_maths.js
+
+// const score = 600
+// const Balance = new Number(100)
+// console.log(Balance);
+
+// console.log(Balance.toString());
+// console.log(Balance.toFixed(2));
+// const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+// const hunderds = 1000000
+// console.log(hunderds.toLocaleString('en-In'));
+
+
+
+// ++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math.abs(-16));
+// console.log(Math.round(4.36));
+// console.log(Math.E.min(4, 3, 6, 8));
+// console.log(Math.E.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min + 1))+min);
+
+
